bca32f9c8f77eccbd35f9effd335e396a8a45df3
leviegu <1821414u@gmail.com>
2022-03-14 07:14:37 +0800

    feat(markdown): use @tailwindcss/typography render it

--- components/markdown-render.tsx
+++ components/markdown-render.tsx
@@ -1,11 +1,25 @@
-import ReactMarkdown from "react-markdown"
+import ReactMarkdown, { Components } from "react-markdown"
 import remarkGfm from "remark-gfm"
 import rehypeRaw from "rehype-raw"
 
+const components = {
+  a: (props: any) => {
+    return (
+      <a href={props.href} target="_blank" rel="noreferrer">
+        {props.children}
+      </a>
+    )
+  },
+}
+
 export default function MarkdownRender({ content }: { content: string }) {
   return (
-    <article>
-      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+    <article className="prose lg:prose-lg prose-a:text-tag-a prose-a:no-underline">
+      <ReactMarkdown
+        components={components}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      >
         {content}
       </ReactMarkdown>
     </article>
