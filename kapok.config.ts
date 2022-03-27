316974b4dc1c8f33833a58f0f4dce4748850f6d2
leviegu <gu.lw@outlook.com>
2022-03-27 19:44:51 +0800

    build: modify build config

--- kapok.config.ts
+++ kapok.config.ts
@@ -1,18 +1,28 @@
 const config = {
+  name: "inn.",
   basic: {
     title: "inn",
   },
   bio: {
+    splicing: "Personal blog by #author#",
     author: "leviegu",
     link: "https://github.com/leviegu",
-    description: "Happiness depends upon ourselves.",
+    slogan: "Happiness depends upon ourselves.",
   },
-  footer: {
-    record: "苏ICP备19014503号",
-    contact: [
-      { id: "github", name: "leviegu", url: "https://github.com/leviegu" },
-    ],
-  },
+  footer: [
+    {
+      id: "github",
+      name: "leviegu",
+      url: "https://github.com/leviegu",
+      label: "github",
+    },
+    {
+      id: "miit",
+      name: "苏ICP备19014503号",
+      url: "https://beian.miit.gov.cn",
+      label: "备案信息",
+    },
+  ],
 }
 
 export default config
