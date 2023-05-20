a6696150e625f5d2540a4972f68851f6547c9db6
leviegu <leviegu@gmail.com>
2023-05-20 22:26:51 +0800

    feat: svelte version

--- components/container.tsx
+++
@@ -1,7 +0,0 @@
-import { memo, ReactNode } from "react"
-
-export default memo(function Container({ children }: { children: ReactNode }) {
-  return (
-    <div className="max-w-3xl mx-auto py-11 px-5 text-primary">{children}</div>
-  )
-})
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+	<title>𝚜𝚞𝚔𝚒</title>
+	<meta name="description" content="suki's website" />
+</svelte:head>
+
+<section>
+	<div class="intro">Hi, I'am sukinee</div>
+</section>
+
+<style>
+	.intro {
+		color: #333333;
+		font-size: 5rem;
+		font-style: italic;
+		font-weight: bold;
+		animation: action 0.5s alternate infinite;
+		user-select: none;
+	}
+
+	@keyframes action {
+		from {
+			text-shadow: 0 0 10px #666666, 0 0 20px #666666, 0 0 30px #666666, 0 0 40px #999999,
+				0 0 50px #999999, 0 0 60px #999999;
+		}
+		to {
+			text-shadow: 0 0 5px #666666, 0 0 10px #666666, 0 0 15px #666666, 0 0 20px #999999,
+				0 0 25px #999999, 0 0 30px #999999;
+		}
+	}
+</style>
