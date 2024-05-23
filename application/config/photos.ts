6cd0714d414ad53496ebec830f05c6eb16c03242
leviegu <gu.lw@foxmail.com>
2024-05-23 14:00:29 +0800

    chore: convert image from jpg to webp

--- application/config/photos.ts
+++ application/config/photos.ts
@@ -6,21 +6,21 @@ function removePrefix(url: string) {
 
 const photos: PhotoItem[] = [
   {
-    url: "/application/public/images/photos/keong-saik-rd-2024-04-22.jpg",
+    url: "/application/public/images/photos/keong-saik-rd-2024-04-22.webp",
     title: "",
-    date: "2024/05/01",
+    date: "2024/04/22",
     location: "Keong Saik Rd.",
   },
   {
-    url: "/application/public/images/photos/s.e.a.-aquarium-2024-04-21.jpg",
+    url: "/application/public/images/photos/s.e.a.-aquarium-2024-04-21.webp",
     title: "jellyfish 🪼",
-    date: "2024/05/01",
+    date: "2024/04/21",
     location: "S.E.A. Aquarium",
   },
   {
-    url: "/application/public/images/photos/waitan-2023-07-21.jpg",
+    url: "/application/public/images/photos/waitan-2023-07-21.webp",
     title: "",
-    date: "2024/05/01",
+    date: "2023/07/21",
     location: "外滩",
   },
 ].map((item) => ({...item, url: removePrefix(item.url)}));
